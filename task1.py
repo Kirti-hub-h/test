f2c4e487e51158a7f6f6df5d0642836136cb7846
Kirti H Mandal <76935707+Kirti-hub-h@users.noreply.github.com>
2025-05-05 22:57:19 +0530

    Update task1.py
    please check

--- task1.py
+++ task1.py
@@ -1,1 +1,9 @@
-print("Yes Sir, Here is your answer")
+print("Yes Sir, Here is your numbers with for loop")
+for i in range (1,11):
+  print(i,end=" ")
+  
+print("\nYes Sir, Here is your numbers with while loop")
+j=1
+while j<11:
+    print(j, end=" ")
+    j=j+1
